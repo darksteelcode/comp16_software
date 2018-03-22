46db54ec166a9e807fce18ac26312d3c9c08e569
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-03-21 20:20:33 -0600

    Added image test program

---
+++ tests/src/txt_rand.asm
@@ -0,0 +1,30 @@
+/* Random Text Generator - darksteelcode
+ * Fill the screen with psuedo-random text
+ */
+#include std.asm\
+pra AX 0;
+prb AX 0;
+
+label loop;
+out AX GFX_TXT_ADDR;
+
+mov AX A OP_*;
+mov AX B OP_*;
+mov RES A OP_^;
+prb B 0xf342;
+pra B 0xf342;
+mov RES A OP_*;
+mov AX B OP_*;
+out RES GFX_TXT_DATA;
+
+prb B 1;
+pra B 1;
+mov AX A OP_+;
+mov RES AX;
+mov RES CND;
+prb CR loop;
+jpc CR loop;
+
+label end;
+prb CR end;
+jmp CR end;

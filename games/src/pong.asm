1e45191f2d496bc6995d630a57f604a27d3d70a5
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-04-17 20:00:50 -0600

    First Pong Commit

---
+++ games/src/pong.asm
@@ -0,0 +1,33 @@
+/* Pong - darksteelcode
+ * A Work in Progress
+ */
+
+#include std.asm\
+#include stdutil.asm\
+
+//Draw in second column
+#macro draw_padel1
+	//AX is char to print
+	put 219 AX;
+	//BX is loc on screen
+	put 1 BX;
+	mv padel1 A OP_*;
+	put 40 B;
+	mv RES A;
+	mv BX B OP_+;
+	mv RES BX;
+	out BX GFX_TXT_ADDR;
+	out AX GFX_TXT_DATA;
+
+\
+
+//Game Logic
+
+
+/Memory Locations
+label w_pressed;
+. 0;
+label s_pressed;
+. 0;
+label padel1;
+. 2;

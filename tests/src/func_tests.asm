fbb1f25149cff11a1a438b0007a608d6cc87d38d
darksteelcode <32946898+darksteelcode@users.noreply.github.com>
2018-04-25 21:53:59 -0600

    print can print formatted chars

--- tests/src/func_tests.asm
+++ tests/src/func_tests.asm
@@ -1,43 +1,23 @@
 #include std.asm\
 #include stdtime.asm\
+#include stdio.hsm\
 
 label PRGM_START;
-
-put 0xff00 SP;
-
-call do_some_recurse 0 40 1;
-//call out_val 0xf0f0;
-
+/*
+call print_char 'a';
+call print_char 'b';
+call print_char '\n';
+call print_char 'c';
+call print_char '\t';
+call print_char 'd';
+call print_char '\b';
+call print_char 'e';
+*/
+call print &str;
 hang!;
 
-func do_some_recurse $i $limit $start {
-	if! $start {
-		mv CR CR OP_-;
-		in A GFX_TXT_ADDR;
-		put 1 B;
-		out RES GFX_TXT_ADDR;
-	};
-	mv CR CR OP_+;
-	in A GFX_TXT_ADDR;
-	put 1 B;
-	out RES GFX_TXT_ADDR;
-	put 65 AX;
-	out AX GFX_TXT_DATA;
+#string str
+Hello
+	Write\
 
-	mv $i A OP_+;
-	put 1 B;
-	mv RES BX OP_+;
-	mv RES B OP_>;
-	mv $limit A OP_>;
-	if! RES {
-		call do_some_recurse BX $limit 0;
-	};
-	out $limit 0;
-};
-
-func out_val $arg1 {
-	local $tmp_str;
-	mv $arg1 $tmp_str;
-	out $tmp_str 0;
-};
 
